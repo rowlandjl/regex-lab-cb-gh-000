de4f04b0f9e4c4e280c0f378f049c7546cf20ce8
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-27 15:20:40 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,5 +15,10 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  phone.each do |number|
+    if !number.match(/\d{4}/)
+      return false
+    end
+  end
+  true
 end
